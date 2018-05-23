801ad66bd8f1f16fec3f84c81dfbe386f7a629e4
laurentabbal <laurent@abbal.com>
2018-05-23 15:00:59 +0800

    finalisation

--- viking.js
+++ viking.js
@@ -3,42 +3,40 @@ appNote = 2, valNote = 2, reaNote = 2, comNote = 2;
 
 function change_coef(coefv, coefn)
 {
-coefn = document.getElementById(coefv).textContent;
-coefn++;
-if (coefn == 4) {
-    coefn = 0;
-}
-document.getElementById(coefv).innerHTML = coefn;
-return coefn;
+  coefn = document.getElementById(coefv).textContent;
+  coefn++;
+  if (coefn == 4) {
+      coefn = 0;
+  }
+  document.getElementById(coefv).innerHTML = coefn;
+  return coefn;
 }
 
 function change_note(notev, noten)
 {
-var noteir = document.getElementById(notev).textContent;
-if (noteir == 'A') {
-  document.getElementById(notev).innerHTML = 'B';
-    noten = 1;
-}
-if (noteir == 'B') {
-  document.getElementById(notev).innerHTML = 'C';
-    noten = -1;
-}
-if (noteir == 'C') {
-  document.getElementById(notev).innerHTML = 'D';
-    noten = -2;
-}
-if (noteir == 'D') {
-  document.getElementById(notev).innerHTML = 'A';
-    noten = 2;
-}
-return noten;
-
+  var noteir = document.getElementById(notev).textContent;
+  if (noteir == 'A') {
+    document.getElementById(notev).innerHTML = 'B';
+      noten = 1;
+  }
+  if (noteir == 'B') {
+    document.getElementById(notev).innerHTML = 'C';
+      noten = -1;
+  }
+  if (noteir == 'C') {
+    document.getElementById(notev).innerHTML = 'D';
+      noten = -2;
+  }
+  if (noteir == 'D') {
+    document.getElementById(notev).innerHTML = 'A';
+      noten = 2;
+  }
+  return noten;
 }
 
 function notefinale(){
   var coefSum = anaCoef + reaCoef + appCoef + comCoef + valCoef;
   var calcn = (anaCoef*anaNote + reaCoef*reaNote + appCoef * appNote + comCoef*comNote + valCoef * valNote + 2 * coefSum)/(2 * coefSum);
   var notefin = Math.round(4 + calcn * 8);
-  document.getElementById("notefinale").innerHTML= notefin+"/20";
-  document.getElementById('notefinale').scrollIntoView();
+  document.getElementById("notefinale").innerHTML= notefin;
 }
